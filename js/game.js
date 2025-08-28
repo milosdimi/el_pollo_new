cc5a7e3d42b0e90e9e4c8ce672b1835e65747953
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-08-29 01:20:22 +0200

    9 - Character anzeigen

--- js/game.js
+++ js/game.js
@@ -1,12 +1,10 @@
 let canvas;
-let ctx;
-let character = new MovableObject();
-
+let world;
 
 function init() {
     canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
+    world = new World(canvas);
 
-    console.log('My Char is', character);
+    console.log('My Char is', world.character);
 
 }
